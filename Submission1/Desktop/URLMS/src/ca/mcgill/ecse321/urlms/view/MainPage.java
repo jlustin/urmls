4ee57b1a9360e39e1106c5510747ac72bf2ce05b
xd00258 <victoriquevuong@gmail.com>
2017-10-08 19:54:25 -0400

    added test view for viewStaffList

--- Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/MainPage.java
+++ Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/MainPage.java
@@ -7,9 +7,26 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import ca.mcgill.ecse321.urlms.controller.Controller;
+import ca.mcgill.ecse321.urlms.model.StaffMember;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class MainPage extends JFrame {
 
 	private JPanel contentPane;
+	JLabel lblNewLabel;
+	
+	public static Controller controller = new Controller();
 
 	/**
 	 * Launch the application.
@@ -35,8 +52,45 @@ public class MainPage extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton btnViewStaffList = new JButton("View Staff List");
+		btnViewStaffList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				List<StaffMember> staffList = controller.viewStaffList();
+				String name;
+				for (StaffMember aMember : staffList) {
+					String previousText = lblNewLabel.getText();
+					name = aMember.getName();
+					lblNewLabel.setText(previousText + name);
+				}
+			}
+		});
+	
+		
+		lblNewLabel = new JLabel("New label");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(133)
+							.addComponent(btnViewStaffList))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel)))
+					.addContainerGap(174, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnViewStaffList)
+					.addGap(18)
+					.addComponent(lblNewLabel)
+					.addContainerGap(199, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
