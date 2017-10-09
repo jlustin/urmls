03a844b5e7b8c44e84d87a497f8b4ce6857a3e1b
JunYu Lei <junyuu@hotmail.com>
2017-10-09 00:52:36 -0400

    attempt at mvc

--- Submission1/Web/displayStaff.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$xml=simplexml_load_file("staff.xml") or die ("Error: Cannot create object");
-	foreach($xml->children() as $staffs){
-		echo $staffs['id'] . "    " . $staffs->name . "<br>";
-	}
-?>
