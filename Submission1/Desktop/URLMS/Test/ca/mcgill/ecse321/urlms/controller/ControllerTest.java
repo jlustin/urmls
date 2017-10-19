5d42963c88b538b686fc48be6f918074364495e6
berri3 <cmliang2004@hotmail.com>
2017-10-19 17:09:05 -0400

    Added comments to the test

--- Submission1/Desktop/URLMS/Test/ca/mcgill/ecse321/urlms/controller/ControllerTest.java
+++ Submission1/Desktop/URLMS/Test/ca/mcgill/ecse321/urlms/controller/ControllerTest.java
@@ -19,21 +19,36 @@ public class ControllerTest {
 	
 	private static URLMS urlms;
 
+	/**
+	 * Set up once before: Load the file
+	 * @throws Exception
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		PersistenceXStream.initializeModelManager("urlms.xml");
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		
 	}
 
+	/**
+	 * Set up the urlms using the Application class
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		urlms = URLMSApplication.getURLMS();
 	}
 
+	/**
+	 * Deletes the urlms
+	 * @throws Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 		urlms.delete();
@@ -44,24 +59,26 @@ public class ControllerTest {
 		//fail("Not yet implemented");
 		StaffManager aStaffManager = urlms.getStaffManager();
 		
+		//check if the staff manager is empty
 		assertEquals(0, aStaffManager.getStaffMembers().size());
 		
-		String name = "Feras";
+		String name = "Feras"; //test name
 		
-		Controller urlmsController = new Controller();
-		urlmsController.addSampleMembers();
+		Controller urlmsController = new Controller(); //create instance of controller
+		urlmsController.addSampleMembers(); //add some sample members
 		
 		//check model in memory
-		assertEquals(3, aStaffManager.getStaffMembers().size());
-		assertEquals(name, aStaffManager.getStaffMember(1).getName());
-		assertEquals(0, (long)urlms.getFundingManager().getTotalBalance());
-		assertEquals(false, (urlms.getInventoryManager().hasInventoryItems()));
+		assertEquals(3, aStaffManager.getStaffMembers().size()); //checks if the staff manager now contains 3 members
+		assertEquals(name, aStaffManager.getStaffMember(1).getName()); //checks if the 2nd member in the list is "Feras"
+		assertEquals(0, (long)urlms.getFundingManager().getTotalBalance()); //check if the balance in the funds is zero
+		assertEquals(false, (urlms.getInventoryManager().hasInventoryItems())); //check if the inventory is empty
 		
+		//save the file
 		urlmsController.save();
-		//load stuff(?)
+		
 		urlms = (URLMS) PersistenceXStream.loadFromXMLwithXStream();
 		
-		//check file contents
+		//check file contents (same checks as above, but with loaded file)
 		assertEquals(3, aStaffManager.getStaffMembers().size());
 		assertEquals(name, aStaffManager.getStaffMember(1).getName());
 		assertEquals(0, (long)urlms.getFundingManager().getTotalBalance());
