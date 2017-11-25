fe0e14beb962dfbd409aa5cfa5d038649dcc4c8d
xd00258 <victoriquevuong@gmail.com>
2017-11-24 21:13:47 -0500

    Pop out pages created
    i just created the pop out pages with nothing inside, they can be used
    to test use cases we implement, just need to add buttons and labels and
    other components.

---
+++ Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/StaffPagePO.java
@@ -0,0 +1,42 @@
+package ca.mcgill.ecse321.urlms.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class StaffPagePO extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StaffPagePO frame = new StaffPagePO();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StaffPagePO() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
