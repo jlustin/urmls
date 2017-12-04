019596b7ab7e5c31031daef3989252a6cc19ceba
JunYu Lei <junyuu@hotmail.com>
2017-12-03 23:22:13 -0500

    continued implementing bootstrap

--- Submission1/Web/index.php
+++ Submission1/Web/index.php
@@ -2,17 +2,60 @@
 	<head>
 		<title>URLMS</title>
 <!-- https://getbootstrap.com/docs/4.0/content/tables/ -->
-<!-- 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
-		<link rel="stylesheet" type="text/css" href="style/homepagecss.css">
-
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<!--  <link rel="stylesheet" type="text/css" href="style/homepagecss.css"> -->
 	</head>
 	<body>
-		<h1 style="text-align: center"><a href="index.php" style="color: white;text-decoration: none;">University Research Lab Management System</a></h1>
+	<div class="container">
+		<h1 style="text-align: center"><a href="index.php">University Research Lab Management System</a></h1>
+		
+		
+		<!-- Bootstrap Template -->
+  		<!-- 	<div class="row">
+    				<div class="col">
+     					1 of 2
+  					</div>
+   					<div class="col">
+      					2 of 2
+    				</div>
+  				</div>
+  				<div class="row">
+    				<div class="col">
+      					1 of 3
+    				</div>
+    				<div class="col">
+      					2 of 3
+    				</div>
+    				<div class="col">
+      					3 of 3
+    				</div>
+  				</div> -->
+		
+		<div class="row">
+    	<ul>
+    		<li>
+	 			<div class="col-sm-4">
+	     			<h2><a href="View/StaffView.php">Staff</a></h2>
+	  			</div>
+  			</li>
+  			<li>
+	  			<div class="col-sm-4">
+	     			<h2><a href="View/InventoryView.php">Inventory</a></h2>
+	  			</div>
+			</li>
+			<li>
+	  			<div class="col-sm-4">
+	     			<h2><a href="View/FundingView.html">Funding</a></h2>
+	  			</div>
+  			</li>
+		</ul>
+  		</div>
 		
 		<ul>
- 				<li><h2><a href="View/StaffView.php">Staff</a></h2></li>
-  				<li><h2><a href="View/InventoryView.php">Inventory</a></h2></li>
-  				<li><h2><a href="View/FundingView.html">Funding</a></h2></li>
+ 			<li><h2><a href="View/StaffView.php">Staff</a></h2></li>
+  			<li><h2><a href="View/InventoryView.php">Inventory</a></h2></li>
+  			<li><h2><a href="View/FundingView.html">Funding</a></h2></li>
 		</ul>
+	</div>
 	</body>
 <html/>
