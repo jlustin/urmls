035f6bf2488fe20ee9cf744566d193b9b70c16de
berri3 <cmliang2004@hotmail.com>
2017-12-07 17:39:29 -0500

    Implemented Sample Exception Checking

--- Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/controller/StaffController.java
+++ Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/controller/StaffController.java
@@ -1,229 +1,265 @@
 package ca.mcgill.ecse321.urlms.controller;
- 
+
 import java.util.*;
- 
+
 import ca.mcgill.ecse321.urlms.application.URLMSApplication;
 import ca.mcgill.ecse321.urlms.model.Lab;
 import ca.mcgill.ecse321.urlms.model.ProgressUpdate;
 
 import ca.mcgill.ecse321.urlms.model.ResearchAssistant;
 import ca.mcgill.ecse321.urlms.model.ResearchAssociate;
-import ca.mcgill.ecse321.urlms.model.ResearchRole;
-
 import ca.mcgill.ecse321.urlms.model.StaffMember;
 import ca.mcgill.ecse321.urlms.model.URLMS;
- 
- 
-public class StaffController extends Controller {
-   
-    /**
-     * This method will get the staff member list of the current urlms in order to view the members
-     * @return the staffList from urlms
-     */
-    public List<StaffMember> viewStaffList(){
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-       
-        List<StaffMember> staffList = aLab.getStaffMembers();
-       
-        return staffList;
-    }
-   
-    //TODO move to JUnit Test
-    /**
-     * This method is used for testing purposes. Three members with names Victor, Feras and Jun2Yu will be
-     * added to the current staff member list. These three members will have different IDs: 123, 111 and 222
-     * respectively.
-     */
-    public void addSampleMembers() {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        StaffMember member = new StaffMember("Victor", 123,123.2, aLab);
-        aLab.addStaffMember(member);
-       
-        StaffMember member2 = new StaffMember("Feras", 111,3232, aLab);
-        aLab.addStaffMember(member2);
-       
-        StaffMember member3 = new StaffMember("Jun2Yu", 222,323, aLab);
-        aLab.addStaffMember(member3);
-    }
-   
-   
-    /**
-     * This method will get the name of a specific staff member in the list and return its name as a string.
-     * @param int index of the staff member in the list
-     * @return name of the staff member as a String
-     */
-    public String viewStaffMember(int index){
-        return URLMSApplication.getURLMS().getLab(0).getStaffMembers().get(index).getName();
-    }
-   
-    //TODO: USE CASES IMPLEMENTATION -----------------------------------------------
-   
-   
-    /** This method will edit the record of a specific staff member by ID
-     * @param id of the staff member by int
-     * box1 and box2 are radio buttons selected by user
-     * box1 == Research Assistant
-     * box2 == Research Associate
-     */
-    public void editStaffmemberRecord(int index, int id, String desiredName, boolean box1, boolean box2, double weeklySalary) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        StaffMember aStaffMember = aLab.getStaffMember(index);
-        aStaffMember.setName(desiredName);
-        aStaffMember.setId(id);
-        aStaffMember.setWeeklySalary(weeklySalary);
-        addRoles(aStaffMember,box1,box2);
- 
-    }
-   
-    /**Edit staff name
-     * @param desired name change by string & id of the staff member by int
-     */
-    public void editStaffmemberName(String desiredName, int id) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        aLab.getStaffMember(id).setName(desiredName);
-    }
-   
-    /** This method will get the progress update of a specific staff member by ID number
-     * @param id of the staff member by int
-     * @return the progress update of the specific member
-     */
-    public List<ProgressUpdate> viewProgressUpdate(int index) {
-       
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        List<ProgressUpdate> progress = aLab.getStaffMember(index).getProgressUpdates();
-        return progress;
-    }
-   
-    /**Add Progress Update
-     * @param desired date by string and desired description by string
-     */
-    public void addProgress(String date, String description, int index) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        StaffMember aStaffMember = aLab.getStaffMember(index);
-        aStaffMember.addProgressUpdate(date, description);
-    }
-   
-    /** This method will get the records of a specific staff member by ID number
-     * @param id of the staff member by int
-     */
-    public void viewStaffMemberRecord(int id) {
-        //will not be used
-    }
-   
-    public String viewStaffMemberName(int index) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        return aLab.getStaffMember(index).getName();
-    }
-    public String viewStaffMemberID(int index) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        return String.valueOf(aLab.getStaffMember(index).getId());
-    }
-   
-   
-    /** This method will add a staff member in the lab
-     * @param name of the staff member by String
-     * and staff Roles defined by booleans for checkedbox/radio buttons
-     * box1==staff assistant
-     * box2==staff associate
-     */
-    public void addStaffMember(String name, boolean box1, boolean box2, double weeklySalary) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        //"auto-unique"
-        int rng = (int)(Math.random()*1000+1);
-        aLab.addStaffMember(name, rng, weeklySalary);
-        addRoles(aLab.getStaffMember(aLab.getStaffMembers().size()-1),box1,box2);
-       
-    }
-   
-   
-   
-    /** This method will remove a staff member in the lab by id
-     * @param id of the staff member by int
-     */
-    public void removeStaffMember(int index) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        aLab.getStaffMember(index).delete();
-       
-    }
-   
-   
-   
-    /** This method will give a summary of the overall URLMS staff
-     * @return a hash map containing all information
-     */
-    public HashMap<String, String> viewStatus() {
-       
-        //TODO: remove this when working on implementation
-        return null;
-    }
-    //helper method for adding roles specificied by checkbox / radio button
-    public void addRoles(StaffMember aStaffMember, boolean box1, boolean box2) {
-        if(aStaffMember.hasResearchRoles()) {
-            aStaffMember.getResearchRole(0).delete();
-        }
-        if(aStaffMember.hasResearchRoles()) {
-            aStaffMember.getResearchRole(0).delete();
-        }
-        //add desired roles
-        if(box1&&box2) {
-            aStaffMember.addResearchRole(new ResearchAssistant("Why you read this", aStaffMember));
-            aStaffMember.addResearchRole(new ResearchAssociate("Why you read this", aStaffMember));
-        }
-        else if(box1&&!box2) {
-            aStaffMember.addResearchRole(new ResearchAssistant("Why you read this", aStaffMember));
-        }
-        else if(box2&&!box1) {
-            aStaffMember.addResearchRole(new ResearchAssociate("Why you read this", aStaffMember));
-        }
-        else {
-            //do nothing
-        }
-    }
-    
-    public void editStaffmemberRecordByID(int id, int newID, String desiredName, boolean box1, boolean box2, double weeklySalary) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        StaffMember aStaffMember = aLab.getStaffMember(0);
-        
-        if(aStaffMember == null) return;
-        
-        for(StaffMember iteratedStaffMember: aLab.getStaffMembers()) {
-        		if(id == iteratedStaffMember.getId()) {
-        			aStaffMember = iteratedStaffMember;
-        		}
-        }
-        
-        aStaffMember.setName(desiredName);
-        aStaffMember.setId(newID);
-        aStaffMember.setWeeklySalary(weeklySalary);
-        addRoles(aStaffMember,box1,box2);
- 
-    }
-    
-    public void removeStaffMemberByID(int id) {
-        URLMS urlms = URLMSApplication.getURLMS();
-        Lab aLab = urlms.getLab(0);
-        StaffMember aStaffMember = aLab.getStaffMember(0);      
-        if(aStaffMember == null) return;
-        
-        for(StaffMember iteratedStaffMember: aLab.getStaffMembers()) {
-    			if(id == iteratedStaffMember.getId()) {
-    					iteratedStaffMember.delete();
-    				}
-        		}
-        
-       
-    }
- 
-}
 
+public class StaffController extends Controller {
+
+	/**
+	 * This method will get the staff member list of the current urlms in order
+	 * to view the members
+	 * 
+	 * @return the staffList from urlms
+	 * @throws InvalidInputException 
+	 */
+	public List<StaffMember> viewStaffList() throws InvalidInputException {
+		String error = "";
+		
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		List<StaffMember> staffList;
+			
+
+		try {
+			staffList = aLab.getStaffMembers();
+			if(staffList.isEmpty()){
+				error = "There are no staff members to display :(";
+			}
+		} catch (RuntimeException e) {
+			throw new InvalidInputException (e.getMessage());
+		}
+
+		if (error.length() > 0){
+			throw new InvalidInputException (error.trim());
+		}
+		return staffList;
+	}
+
+	// TODO move to JUnit Test
+	/**
+	 * This method is used for testing purposes. Three members with names
+	 * Victor, Feras and Jun2Yu will be added to the current staff member list.
+	 * These three members will have different IDs: 123, 111 and 222
+	 * respectively.
+	 */
+	public void addSampleMembers() {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		StaffMember member = new StaffMember("Victor", 123, 123.2, aLab);
+		aLab.addStaffMember(member);
+
+		StaffMember member2 = new StaffMember("Feras", 111, 3232, aLab);
+		aLab.addStaffMember(member2);
+
+		StaffMember member3 = new StaffMember("Jun2Yu", 222, 323, aLab);
+		aLab.addStaffMember(member3);
+	}
+
+	/**
+	 * This method will get the name of a specific staff member in the list and
+	 * return its name as a string.
+	 * 
+	 * @param int
+	 *            index of the staff member in the list
+	 * @return name of the staff member as a String
+	 */
+	public String viewStaffMember(int index) {
+		return URLMSApplication.getURLMS().getLab(0).getStaffMembers().get(index).getName();
+	}
+
+	// TODO: USE CASES IMPLEMENTATION
+	// -----------------------------------------------
+
+	/**
+	 * This method will edit the record of a specific staff member by ID
+	 * 
+	 * @param id
+	 *            of the staff member by int box1 and box2 are radio buttons
+	 *            selected by user box1 == Research Assistant box2 == Research
+	 *            Associate
+	 */
+	public void editStaffmemberRecord(int index, int id, String desiredName, boolean box1, boolean box2,
+			double weeklySalary) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		StaffMember aStaffMember = aLab.getStaffMember(index);
+		aStaffMember.setName(desiredName);
+		aStaffMember.setId(id);
+		aStaffMember.setWeeklySalary(weeklySalary);
+		addRoles(aStaffMember, box1, box2);
+
+	}
+
+	/**
+	 * Edit staff name
+	 * 
+	 * @param desired
+	 *            name change by string & id of the staff member by int
+	 */
+	public void editStaffmemberName(String desiredName, int id) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		aLab.getStaffMember(id).setName(desiredName);
+	}
+
+	/**
+	 * This method will get the progress update of a specific staff member by ID
+	 * number
+	 * 
+	 * @param id
+	 *            of the staff member by int
+	 * @return the progress update of the specific member
+	 */
+	public List<ProgressUpdate> viewProgressUpdate(int index) {
+
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		List<ProgressUpdate> progress = aLab.getStaffMember(index).getProgressUpdates();
+		return progress;
+	}
+
+	/**
+	 * Add Progress Update
+	 * 
+	 * @param desired
+	 *            date by string and desired description by string
+	 */
+	public void addProgress(String date, String description, int index) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		StaffMember aStaffMember = aLab.getStaffMember(index);
+		aStaffMember.addProgressUpdate(date, description);
+	}
+
+	/**
+	 * This method will get the records of a specific staff member by ID number
+	 * 
+	 * @param id
+	 *            of the staff member by int
+	 */
+	public void viewStaffMemberRecord(int id) {
+		// will not be used
+	}
+
+	public String viewStaffMemberName(int index) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		return aLab.getStaffMember(index).getName();
+	}
+
+	public String viewStaffMemberID(int index) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		return String.valueOf(aLab.getStaffMember(index).getId());
+	}
+
+	/**
+	 * This method will add a staff member in the lab
+	 * 
+	 * @param name
+	 *            of the staff member by String and staff Roles defined by
+	 *            booleans for checkedbox/radio buttons box1==staff assistant
+	 *            box2==staff associate
+	 */
+	public void addStaffMember(String name, boolean box1, boolean box2, double weeklySalary) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		// "auto-unique"
+		int rng = (int) (Math.random() * 1000 + 1);
+		aLab.addStaffMember(name, rng, weeklySalary);
+		addRoles(aLab.getStaffMember(aLab.getStaffMembers().size() - 1), box1, box2);
+
+	}
+
+	/**
+	 * This method will remove a staff member in the lab by id
+	 * 
+	 * @param id
+	 *            of the staff member by int
+	 */
+	public void removeStaffMember(int index) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		aLab.getStaffMember(index).delete();
+
+	}
+
+	/**
+	 * This method will give a summary of the overall URLMS staff
+	 * 
+	 * @return a hash map containing all information
+	 */
+	public HashMap<String, String> viewStatus() {
+
+		// TODO: remove this when working on implementation
+		return null;
+	}
+
+	// helper method for adding roles specificied by checkbox / radio button
+	public void addRoles(StaffMember aStaffMember, boolean box1, boolean box2) {
+		if (aStaffMember.hasResearchRoles()) {
+			aStaffMember.getResearchRole(0).delete();
+		}
+		if (aStaffMember.hasResearchRoles()) {
+			aStaffMember.getResearchRole(0).delete();
+		}
+		// add desired roles
+		if (box1 && box2) {
+			aStaffMember.addResearchRole(new ResearchAssistant("Why you read this", aStaffMember));
+			aStaffMember.addResearchRole(new ResearchAssociate("Why you read this", aStaffMember));
+		} else if (box1 && !box2) {
+			aStaffMember.addResearchRole(new ResearchAssistant("Why you read this", aStaffMember));
+		} else if (box2 && !box1) {
+			aStaffMember.addResearchRole(new ResearchAssociate("Why you read this", aStaffMember));
+		} else {
+			// do nothing
+		}
+	}
+
+	public void editStaffmemberRecordByID(int id, int newID, String desiredName, boolean box1, boolean box2,
+			double weeklySalary) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		StaffMember aStaffMember = aLab.getStaffMember(0);
+
+		//if there are currently no staff members in the lab
+		if (aStaffMember == null)
+			return;
+
+		for (StaffMember iteratedStaffMember : aLab.getStaffMembers()) {
+			if (id == iteratedStaffMember.getId()) {
+				aStaffMember = iteratedStaffMember;
+			}
+		}
+
+		aStaffMember.setName(desiredName);
+		aStaffMember.setId(newID);
+		aStaffMember.setWeeklySalary(weeklySalary);
+		addRoles(aStaffMember, box1, box2);
+
+	}
+
+	public void removeStaffMemberByID(int id) {
+		URLMS urlms = URLMSApplication.getURLMS();
+		Lab aLab = urlms.getLab(0);
+		StaffMember aStaffMember = aLab.getStaffMember(0);
+		if (aStaffMember == null)
+			return;
+
+		for (StaffMember iteratedStaffMember : aLab.getStaffMembers()) {
+			if (id == iteratedStaffMember.getId()) {
+				iteratedStaffMember.delete();
+			}
+		}
+
+	}
+
+}
--- Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/StaffPagePO.java
+++ Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/StaffPagePO.java
@@ -1,63 +1,48 @@
 package ca.mcgill.ecse321.urlms.view;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-import ca.mcgill.ecse321.urlms.application.URLMSApplication;
-import ca.mcgill.ecse321.urlms.controller.Controller;
+import ca.mcgill.ecse321.urlms.controller.InvalidInputException;
 import ca.mcgill.ecse321.urlms.controller.StaffController;
 import ca.mcgill.ecse321.urlms.model.StaffMember;
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.List;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-import javax.swing.AbstractButton;
-import javax.swing.BoxLayout;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
-import javax.swing.JTextField;
-import javax.swing.JCheckBox;
-
 public class StaffPagePO extends JFrame {
+	//JLabels
 	JLabel staffMemberListLabel;
 	JLabel feelFreeLabel;
-	private JPanel contentPane;
 	JLabel welcomeToStaffLabel;
+	
+	//JButtons
+	JButton btnAddStaff;
+	JButton btnEditStaffMember;
+	JButton btnViewStaffList;
+	JButton btnSave;
+	private JButton btnClose;
+	
+	//Pop out pages
 	public AddStaffMemberPO asmpo = new AddStaffMemberPO();
 	public EditStaffMemberPO esmpo = new EditStaffMemberPO();
 	
+	//JPanel
+	private JPanel contentPane;
+
+	//controller	
 	public static StaffController controller = new StaffController();
-
 	
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					StaffPagePO frame = new StaffPagePO();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	//error
+	private String error;
+	
 
 	/**
 	 * Create the frame.
@@ -78,20 +63,15 @@ public class StaffPagePO extends JFrame {
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.SOUTH);
-		JButton btnAddStaff = new JButton("Add Staff");
+		btnAddStaff = new JButton("Add Staff");
 		btnAddStaff.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				asmpo.setVisible(true);
-//				String staffMemberName = txtName.getText();
-//				boolean box1 = ResearchAssistantBox.isSelected();
-//				boolean box2 = ResearchAssociateBox.isSelected();
-//				
-//				controller.addStaffMember(staffMemberName, box1, box2);
 			}
 		});
 		panel.add(btnAddStaff);
 		
-		JButton btnEditStaffMember = new JButton("Edit Staff Member");
+		btnEditStaffMember = new JButton("Edit Staff Member");
 		btnEditStaffMember.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				esmpo.setVisible(true);
@@ -99,30 +79,38 @@ public class StaffPagePO extends JFrame {
 		});
 		panel.add(btnEditStaffMember);
 		
-		JButton btnViewStaffList = new JButton("View Staff List");
+		btnViewStaffList = new JButton("View Staff List");
 		panel.add(btnViewStaffList);
 		btnViewStaffList.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				staffMemberListLabel.setText("");
-				List<StaffMember> staffList = controller.viewStaffList();
-				String name;
-				int id;
-				staffMemberListLabel.setText("<html>");
-				for (StaffMember aMember : staffList) {
+				error = "";
+				
+				
+				staffMemberListLabel.setText("");				
+				try {
+					List<StaffMember> staffList = controller.viewStaffList();
+					String name;
+					int id;
+					staffMemberListLabel.setText("<html>");
+					for (StaffMember aMember : staffList) {
+						String previousText = staffMemberListLabel.getText();
+						name = aMember.getName();
+						id = aMember.getId();
+						staffMemberListLabel.setText(previousText + name + " " + id + " <br/>");
+					}
 					String previousText = staffMemberListLabel.getText();
-					name = aMember.getName();
-					id = aMember.getId();
-					staffMemberListLabel.setText(previousText + name + " " + id + " <br/>");
+					staffMemberListLabel.setText(previousText + "</html>");
+				} catch (InvalidInputException e1) {
+					error = e1.getMessage();
 				}
-				String previousText = staffMemberListLabel.getText();
-				staffMemberListLabel.setText(previousText + "</html>");
+				refreshData();
 			}
 		});
 		
-		JButton btnSave = new JButton("Save");
+		btnSave = new JButton("Save");
 		panel.add(btnSave);
 		
-		JButton btnClose = new JButton("Close");
+		btnClose = new JButton("Close");
 		btnClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				close();
@@ -147,40 +135,6 @@ public class StaffPagePO extends JFrame {
 			welcomeToStaffLabel = new JLabel("Welcome to Staff. There's a lot of stuff.");
 			panel_1.add(welcomeToStaffLabel);
 			
-//			txtName = new JTextField();
-//			txtName.setText("Name");
-//			txtName.setColumns(10);
-			
-			
-//			GroupLayout gl_contentPane = new GroupLayout(contentPane);
-//			gl_contentPane.setHorizontalGroup(
-//				gl_contentPane.createParallelGroup(Alignment.LEADING)
-//					.addGroup(gl_contentPane.createSequentialGroup()
-//						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-//							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 525, GroupLayout.PREFERRED_SIZE)
-//							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 525, GroupLayout.PREFERRED_SIZE)
-//							.addGroup(gl_contentPane.createSequentialGroup()
-//								.addGap(30)
-//								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-//									.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-//									.addComponent(ResearchAssistantBox)
-//									.addComponent(ResearchAssociateBox))))
-//						.addGap(19))
-//			);
-//			gl_contentPane.setVerticalGroup(
-//				gl_contentPane.createParallelGroup(Alignment.LEADING)
-//					.addGroup(gl_contentPane.createSequentialGroup()
-//						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-//						.addGap(34)
-//						.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-//						.addGap(18)
-//						.addComponent(ResearchAssistantBox)
-//						.addGap(18)
-//						.addComponent(ResearchAssociateBox)
-//						.addGap(61)
-//						.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-//			);
-//			contentPane.setLayout(gl_contentPane);
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controller.save();
@@ -190,7 +144,12 @@ public class StaffPagePO extends JFrame {
 	
 	
 	private void refreshData(){
-		
+		if(error.length() > 0){
+			feelFreeLabel.setText(error);
+		}
+		else{
+			feelFreeLabel.setText("Feel free to try adding some staff, and then viewing them.");
+		}
 	}
 	
 	public void close() { 
