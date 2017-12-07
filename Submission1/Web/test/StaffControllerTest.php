da6fe080b4cb9e76190d69c1d4336bda0dfb5a2f
JunYu Lei <junyuu@hotmail.com>
2017-12-07 17:18:11 -0500

    Added tests for staff controller

--- Submission1/Web/test/StaffControllerTest.php
+++ Submission1/Web/test/StaffControllerTest.php
@@ -30,6 +30,9 @@
 		{
 		}
 	
+		/*
+		 * Add Staff Tests - Done
+		 */
 		public function testAddStaffMember()
 		{
 			// 1. Create test data
@@ -51,6 +54,89 @@
 			$this->assertEquals(1, count($this->urlms->getLab_index(0)->getStaffMembers()));
 			$myStaff = $this->urlms->getLab_index(0)->getStaffMember_index(0);
 			$this->assertEquals("bob", $myStaff->getName());
+		}
+		
+		public function testAddStaffMemberNullName()
+		{
+			// 1. Create test data
+			try{
+				$this->controller->addStaff(null, 90);
+			}catch (Exception $e) {
+				$this->assertEquals("Please enter a valid name.", $e->getMessage()); 
+			}
+			
+			// 2. Write all of the data
+			$pers = $this->p;
+			$pers->writeDataToStore($this->urlms);
+			
+			// 3. Clear the data from memory
+			$this->urlms->delete();
+			
+			$this->assertEquals(0, $this->urlms->numberOfLabs());
+			
+			// 4. Load it back in
+			$this->urlms = $pers->loadDataFromStore();
+			
+			// 5. Check that we got it back
+			$this->assertEquals(0, count($this->urlms->getLab_index(0)->getStaffMembers()));
+// 			$myStaff = $this->urlms->getLab_index(0)->getStaffMember_index(0);
+// 			$this->assertEquals("bob", $myStaff->getName());
+		}
+		
+		public function testAddStaffMemberNullSalary()
+		{
+			// 1. Create test data
+			try{
+				$this->controller->addStaff("jasmine", null);
+			}catch (Exception $e) {
+				$this->assertEquals("Please enter a valid number for the salary.", $e->getMessage());
+			}
+			
+			// 2. Write all of the data
+			$pers = $this->p;
+			$pers->writeDataToStore($this->urlms);
+			
+			// 3. Clear the data from memory
+			$this->urlms->delete();
+			
+			$this->assertEquals(0, $this->urlms->numberOfLabs());
+			
+			// 4. Load it back in
+			$this->urlms = $pers->loadDataFromStore();
+			
+			// 5. Check that we got it back
+			$this->assertEquals(0, count($this->urlms->getLab_index(0)->getStaffMembers()));
+			// 			$myStaff = $this->urlms->getLab_index(0)->getStaffMember_index(0);
+			// 			$this->assertEquals("bob", $myStaff->getName());
+		}
+		
+		/*
+		 *Remove Staff 
+		 */
+		public function testRemoveStaff()
+		{
+			// 1. Create test data
+			$newStaffMember = new StaffMember("jasmine", rand(0,1000), 100, $this->urlms->getLab_index(0));
+			$this->urlms->getLab_index(0)->addStaffMember($newStaffMember);
+			
+			$this->controller->removeStaff("jasmine", $newStaffMember->getId());
+			
+			// 2. Write all of the data
+			$pers = $this->p;
+			$pers->writeDataToStore($this->urlms);
+			
+			// 3. Clear the data from memory
+			$this->urlms->delete();
+			
+			$this->assertEquals(0, $this->urlms->numberOfLabs());
+			
+			// 4. Load it back in
+			$this->urlms = $pers->loadDataFromStore();
+			
+			// 5. Check that we got it back
+			$this->assertEquals(0, count($this->urlms->getLab_index(0)->getStaffMembers()));
+			// 			$myStaff = $this->urlms->getLab_index(0)->getStaffMember_index(0);
+			// 			$this->assertEquals("bob", $myStaff->getName());
 		}
 	
 	}
