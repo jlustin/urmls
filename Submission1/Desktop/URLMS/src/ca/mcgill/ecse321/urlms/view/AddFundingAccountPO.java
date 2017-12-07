88581f0dc7a88d7a0a983fa6d405f228d5f14c50
xd00258 <victoriquevuong@gmail.com>
2017-12-07 17:51:56 -0500

    changed everything to Dispose on Close instead of exiting LMAO

--- Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/AddFundingAccountPO.java
+++ Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/AddFundingAccountPO.java
@@ -44,7 +44,7 @@ public class AddFundingAccountPO extends JFrame {
 	 */
 	public AddFundingAccountPO() {
 		setTitle("Add Funding Account");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/AddInventoryItemPO.java
+++ Submission1/Desktop/URLMS/src/ca/mcgill/ecse321/urlms/view/AddInventoryItemPO.java
@@ -47,7 +47,7 @@ public class AddInventoryItemPO extends JFrame {
 	 */
 	public AddInventoryItemPO() {
 		setTitle("Add Inventory Item");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
